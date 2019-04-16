91bd67ef453273cd996df44d2c1142c203a7e6fb
rachelwong <hello.rachelwong@gmail.com>
2019-04-16 15:56:08 +1000

    updated contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+    # validates FILL_IN, presence: true
+    validates :name, length: {minimum: 5}, presence: true
+    validates :address, length: {minimum: 5}, presence: true
+    validates :city, length: {minimum: 3}, presence: true
+    validates :phone, length: {minimum: 8}, presence: true
 end
